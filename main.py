3755a22e5835ec378864ee9da00de7e851ee4bf7
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2015-09-09 14:00:53 -0700

    uses pseudo random generators to place the modified pixels

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def valid_file(param):
 
 parser = argparse.ArgumentParser(description='Encrypt and decrypt messages and hide them in an image.')
 parser.add_argument('-i', dest="image", type=valid_file, metavar="image", help='Location of image you wish to decrypt or encrypt to.', required=True)
-parser.add_argument('-m', dest="message", metavar="message", help='Filepath of message you want to encrypt.', default="check_string_for_empty")
+parser.add_argument('-m', dest="message", metavar="message", help='Message you want to encrypt.', default="check_string_for_empty")
 parser.add_argument('-p', dest="password", metavar="password", help='Password (for decryption)')
 args = parser.parse_args()
 crypt = Encrypter()
@@ -24,25 +24,30 @@ def encrypt():
 	im = Image.open("images/"+str(random.randint(0, 5))+".jpg")
 	pix = im.load()
 
-	encrypted, pwd = crypt.encrypt(open(args.message).read(), 10)
+	encrypted, pwd = crypt.encrypt(args.message, random.randint(1, 30))
 	x=0
-	counter = 0
+	seed = pwd.split('.')[0]
+	random.seed(seed)
 	if (len(encrypted) % 3 == 0):
-		encrypted+="\x00"
+		encrypted+=chr(0)
+		encrypted+=chr(0)
+		encrypted+=chr(0)
 	while (x < len(encrypted)):
 		r = ord(encrypted[x])
 		if (x+1 >= len(encrypted)):
+			print "zero"
 			g = 0
 		else: 
 			g = ord(encrypted[x+1])
 		if (x+2 >= len(encrypted)):
+			print "zero"
 			b = 0
 		else: 
 			b = ord(encrypted[x+2])
 		print (r, g, b)
-		pix[0, counter] = (r, g, b)
+		i, y = random.randint(0, im.size[0]-1), random.randint(0, im.size[1]-1)
+		pix[i, y] = (r, g, b)
 		x+=3
-		counter += 1
 	im.save(args.image)
 	print "Here's your password for decryption " + pwd
 #	with open(args.image, "a") as f:
@@ -54,18 +59,19 @@ def decrypt():
 			pass
 		last = line
 	pwd = args.password
+	seed = pwd.split('.')[0]
+	random.seed(seed)
 	im = Image.open(args.image)
 	pix = im.load()
 	encrypted = ""
-	i = 0
 	notdone = True
 	while notdone:
-		for x in range(0, 3):
-			if (pix[0, i][x] != 0):
-				encrypted += chr(pix[0, i][x])
+		x, y = random.randint(0, im.size[0]-1), random.randint(0, im.size[1]-1)
+		for i in range(0, 3):
+			if (pix[x, y][i] != 0):
+				encrypted += chr(pix[x, y][i])
 			else:
 				notdone = False
-		i+=1
 	print "Done! Here's the message:\n" + crypt.decrypt(encrypted, pwd)
 
 if (args.message == "check_string_for_empty"):
